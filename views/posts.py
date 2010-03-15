2bed8ea1f61b4f143726d6b987725fc3fe1e1d30
Wes Rogers <wesrog@gmail.com>
2010-03-14 23:12:03 -0700

    no need to create all the attributes. create them dynamically

--- views/posts.py
+++ views/posts.py
@@ -4,6 +4,13 @@ from models.post import Post
 class Base(pystache.View):
   template_path = 'views/posts'
 
+  def __getattr__(self, name):
+    if self.post:
+      try:
+        return self.post[name]
+      except:
+        AttributeError
+
 class Index(Base):
   def posts(self):
     return Post.all()
@@ -11,14 +18,4 @@ class Index(Base):
 class Show(Base):
   post = None
 
-  def title(self):
-    if self.post:
-      return self.post['title']
 
-  def author(self):
-    if self.post:
-      return self.post['author']
-
-  def body(self):
-    if self.post:
-      return self.post['body']
